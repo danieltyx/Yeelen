40f41960f2aa316752a8a3d0d812829bffb91f31
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2023-10-21 19:20:08 -0400

    APNS

--- front/Yeelen/Yeelen/YeelenApp.swift
+++ front/Yeelen/Yeelen/YeelenApp.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
-import PushKit
+import UserNotifications
 
 @main
 struct YeelenApp: App {
+    @UIApplicationDelegateAdaptor var delegate: AppDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -17,6 +19,67 @@ struct YeelenApp: App {
     }
 }
 
-//class AppDelegate: NSObject, UIApplicationDelegate, PKPushRegistryDelegate {
-//    
-//}
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+
+        registerForPushNotifications()
+
+        UIApplication.shared.registerForRemoteNotifications()
+
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        registryToken(deviceToken: deviceToken)
+    }
+
+    func application(
+        _ application: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: Error
+    ) {
+        print("Failed to register: \(error)")
+    }
+
+    func registryToken(deviceToken: Data) {
+        print("dooo")
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        let pastToken = UserDefaults(suiteName: "group.zhuhaoyu.yeelen")?.string(forKey: "pushNotificationToken")
+        if let pastToken {
+            print("Past Token: \(pastToken)")
+            if pastToken != token {
+                UserDefaults(suiteName: "group.zhuhaoyu.yeelen")?.set(token, forKey: "pushNotificationToken")
+            }
+        } else {
+            UserDefaults(suiteName: "group.zhuhaoyu.yeelen")?.set(token, forKey: "pushNotificationToken")
+        }
+        print("Device Token: \(token)")
+    }
+
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert]) { granted, _ in
+                print("Permission granted: \(granted)")
+            }
+    }
+
+    func application(
+        _ application: UIApplication,
+        didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+    ) {
+        guard let aps = userInfo["aps"] as? [String: AnyObject] else {
+            completionHandler(.failed)
+            return
+        }
+        print(aps)
+    }
+
+}
