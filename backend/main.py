a37d1680625a93e9e6c79fc09ef58d04123c3e54
Anubhav Agarwal <anubhava@umich.edu>
2023-10-20 23:27:35 -0400

    Remove unnecessary socket.io boilerplate code (routes for websites - no need for website)

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,6 @@ sio = socketio.AsyncServer()
 app = web.Application()
 sio.attach(app)
 
-async def index(request):
-    """Serve the client-side application."""
-    with open('index.html') as f:
-        return web.Response(text=f.read(), content_type='text/html')
-
 @sio.event
 def connect(sid, environ):
     print("connect ", sid)
@@ -21,9 +16,6 @@ async def chat_message(sid, data):
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
-
-app.router.add_static('/static', 'static')
-app.router.add_get('/', index)
 
 if __name__ == '__main__':
     web.run_app(app)
