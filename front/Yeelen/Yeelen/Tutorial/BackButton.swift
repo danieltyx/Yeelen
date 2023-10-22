7a58247c3006f70937380176221e21c839f40631
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2023-10-21 21:09:11 -0400

    torturial

---
+++ front/Yeelen/Yeelen/Tutorial/BackButton.swift
@@ -0,0 +1,18 @@
+//
+//  BackButton.swift
+//  Yeelen
+//
+//  Created by 朱浩宇 on 2023/10/21.
+//
+
+import SwiftUI
+
+struct BackButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackButton()
+}
