afe39a12c087ebc80275a06bfa7606b740a534b9
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2023-10-22 01:54:09 -0400

    in progress view

---
+++ front/Yeelen/Yeelen/InProgress/InProgressView.swift
@@ -0,0 +1,18 @@
+//
+//  InProgressView.swift
+//  Yeelen
+//
+//  Created by 朱浩宇 on 2023/10/22.
+//
+
+import SwiftUI
+
+struct InProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InProgressView()
+}
---
+++ front/Yeelen/Yeelen/InProgress/ProgressWrappedView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressWrappedView.swift
+//  Yeelen
+//
+//  Created by 朱浩宇 on 2023/10/22.
+//
+
+import SwiftUI
+
+struct ProgressWrappedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressWrappedView()
+}
